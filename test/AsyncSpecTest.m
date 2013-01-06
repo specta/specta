9982f37d818f72b6ec7091fbd384725abf7f7fea
Dan Palmer <dan.palmer@me.com>
2013-01-06 23:04:16 +0000

    Added a basic test for asynchronous support

---
+++ test/AsyncSpecTest.m
@@ -0,0 +1,24 @@
+#import "TestHelper.h"
+
+SpecBegin(_AsyncSpecTest)
+
+describe(@"group", ^{
+  it_(@"example 1", ^(void (^done)()) {
+    dispatch_async(dispatch_get_main_queue(), ^{
+      expect(YES).toBeFalsy();
+    });
+  });
+});
+
+SpecEnd
+
+@interface AsyncSpecTest : SenTestCase; @end
+@implementation AsyncSpecTest
+
+- (void)testAsyncSpec {
+  SenTestRun *result = RunSpec(_AsyncSpecTestSpec);
+  expect([result failureCount]).toEqual(1);
+  expect([result hasSucceeded]).toEqual(NO);
+}
+
+@end
