16d72384f4f123ee4e25a55f41a322267d351839
Peter Jihoon Kim <raingrove@gmail.com>
2013-01-09 23:18:41 +0800

    fixed async spec test failing

--- test/AsyncSpecTest.m
+++ test/AsyncSpecTest.m
@@ -1,11 +1,30 @@
 #import "TestHelper.h"
 
+static NSString
+  *foo = @"foo"
+, *bar = @"bar"
+;
+
 SpecBegin(_AsyncSpecTest)
 
 describe(@"group", ^{
   it_(@"example 1", ^(void (^done)()) {
     dispatch_async(dispatch_get_main_queue(), ^{
-      expect(YES).toBeFalsy();
+      expect(foo).toEqual(@"foo");
+      done();
+    });
+  });
+
+  it_(@"example 2", ^(void (^done)()) {
+    dispatch_async(dispatch_get_main_queue(), ^{
+      expect(bar).toEqual(@"bar");
+      done();
+    });
+  });
+
+  it_(@"example 3", ^(void (^done)()) {
+    dispatch_async(dispatch_get_main_queue(), ^{
+      expect(NO).toBeFalsy();
       done();
     });
   });
@@ -17,9 +36,13 @@ SpecEnd
 @implementation AsyncSpecTest
 
 - (void)testAsyncSpec {
+  foo = @"not foo";
+  bar = @"not bar";
   SenTestRun *result = RunSpec(_AsyncSpecTestSpec);
-  expect([result failureCount]).toEqual(1);
+  expect([result failureCount]).toEqual(2);
   expect([result hasSucceeded]).toEqual(NO);
+  foo = @"foo";
+  bar = @"bar";
 }
 
 @end
