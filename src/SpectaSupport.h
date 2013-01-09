1babf4df8849308876f33271c0a653e2464342d7
Peter Jihoon Kim <raingrove@gmail.com>
2013-01-10 03:52:09 +0800

    fail instead of skipping when exceptions get thrown outside actual tests - fixes #15

--- src/SpectaSupport.h
+++ src/SpectaSupport.h
@@ -7,10 +7,23 @@
 @end \
 @implementation name##Spec \
 - (void)SPT_defineSpec { \
-  [self SPT_setCurrentSpecWithFileName:(file) lineNumber:(line)];
+  const char *SPT_specFileName = file; \
+  @try { \
+    [self SPT_setCurrentSpecWithFileName:(file) lineNumber:(line)];
 
 #define _SPT_SpecEnd \
-  [self SPT_unsetCurrentSpec]; \
+    [self SPT_unsetCurrentSpec]; \
+  } @catch(NSException *exception) { \
+    fprintf(stderr, "%s: An exception has occured outside of tests, aborting.\n\n%s (%s) \n", SPT_specFileName, [[exception name] UTF8String], [[exception reason] UTF8String]); \
+    if([exception respondsToSelector:@selector(callStackSymbols)]) { \
+      NSArray *callStackSymbols = [exception callStackSymbols]; \
+      if(callStackSymbols) { \
+        NSString *callStack = [NSString stringWithFormat:@"\n  Call Stack:\n    %@\n", [callStackSymbols componentsJoinedByString:@"\n    "]]; \
+        fprintf(stderr, "%s", [callStack UTF8String]); \
+      } \
+    } \
+    exit(1); \
+  } \
 } \
 @end
 
