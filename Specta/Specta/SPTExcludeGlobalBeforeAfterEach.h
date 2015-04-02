c6e0a4311bb1e610532c1c7aa6e62ddd26479aed
Eric Watson <wasnotrice@gmail.com>
2015-04-02 13:09:07 -0500

    Add message to deprecation warning

--- Specta/Specta/SPTExcludeGlobalBeforeAfterEach.h
+++ Specta/Specta/SPTExcludeGlobalBeforeAfterEach.h
@@ -5,6 +5,6 @@
 
 // This protocol was used for blacklisting classes for global beforeEach and afterEach blocks.
 // Now, instead, classes are whitelisted by implementing the SPTGlobalBeforeAfterEach protocol.
-__deprecated
+__deprecated_msg("Please whitelist classes instead with the SPTGlobalBeforeAfterEach protocol")
 @protocol SPTExcludeGlobalBeforeAfterEach <NSObject>
 @end
